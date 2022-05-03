9f8d02a07e9ab7d99e89bd03a511fcde5dbb30c7
Лобова Дарья Сергеевна <94217366+darialobova@users.noreply.github.com>
2022-05-03 21:56:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,8 +5,8 @@ int countPairs1(int *arr, int len, int value) {
     for (int j = i + 1; j < len; j++) {
       if (arr[j] + arr[i] == value)
         count++;
-    }  
-  }  
+    }
+  }
   return count;
 }
 
@@ -17,8 +17,8 @@ int countPairs2(int *arr, int len, int value) {
       for (int j = len - 1; j > i; j--) {
         if (arr[i] + arr[j] == value)
           count++;
-      }  
-    }  
+      }
+    }
   }
   return count;
 }
@@ -32,23 +32,23 @@ int countPairs3(int *arr, int len, int value) {
       int m = (left + right) / 2;
       if (arr[i] + arr[m] == value) {
         count++;
-        if j = m + 1;
+        int j = m + 1;
         while (arr[i] + arr[j] == value && j < right) {
           count++;
           j++;
-        } 
+        }
         j = m - 1;
         while (arr[i] + arr[j] == value && j > left) {
           count++;
           j--;
-        }  
+        }
         break;
-      } 
+      }
       if (arr[i] + arr[m] > value
         right = m;
       else
         left = m;
-    }  
-  }  
+    }
+  }
   return count;
 }
